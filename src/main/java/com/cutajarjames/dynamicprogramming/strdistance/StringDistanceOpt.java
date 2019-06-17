85b43d0839a10bf8e888192883aa35cba1c7b146
cutajarj2 <james.cutajar@blip.pt>
2019-06-17 09:47:07 +0100

    renaming variable to match slides

--- src/main/java/com/cutajarjames/dynamicprogramming/strdistance/StringDistanceOpt.java
+++ src/main/java/com/cutajarjames/dynamicprogramming/strdistance/StringDistanceOpt.java
@@ -6,35 +6,35 @@ import java.util.Collections;
 public class StringDistanceOpt {
     private String strA;
     private String strB;
-    private int[] dist1;
-    private int[] dist2;
+    private int[] distRead;
+    private int[] distWrite;
 
     public StringDistanceOpt(String strA, String strB) {
         this.strA = strA;
         this.strB = strB;
-        this.dist1 = new int[strB.length() + 1];
-        this.dist2 = new int[strB.length() + 1];
+        this.distRead = new int[strB.length() + 1];
+        this.distWrite = new int[strB.length() + 1];
 
         for (int a = 0; a <= strA.length(); a++) {
             for (int b = 0; b <= strB.length(); b++) {
-                if (a == 0) this.dist2[b] = b;
-                else if (b == 0) this.dist2[b] = a;
+                if (a == 0) this.distWrite[b] = b;
+                else if (b == 0) this.distWrite[b] = a;
                 else {
                     int replaceCost = strA.charAt(a - 1) == strB.charAt(b - 1) ? 0 : 1;
-                    int costDelete = this.dist1[b] + 1;
-                    int costInsert = this.dist2[b - 1] + 1;
-                    int costReplace = this.dist1[b - 1] + replaceCost;
-                    this.dist2[b] = Collections.min(Arrays.asList(costDelete, costInsert, costReplace));
+                    int costDelete = this.distRead[b] + 1;
+                    int costInsert = this.distWrite[b - 1] + 1;
+                    int costReplace = this.distRead[b - 1] + replaceCost;
+                    this.distWrite[b] = Collections.min(Arrays.asList(costDelete, costInsert, costReplace));
                 }
             }
-            int[] temp = dist1;
-            dist1 = dist2;
-            dist2 = temp;
+            int[] temp = distRead;
+            distRead = distWrite;
+            distWrite = temp;
         }
     }
 
     public int distance() {
-        return this.dist1[strB.length()];
+        return this.distRead[strB.length()];
     }
 
     public static void main(String[] args) {
