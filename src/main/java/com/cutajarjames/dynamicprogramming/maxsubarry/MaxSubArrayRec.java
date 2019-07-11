e09f816638a180ee672a0d5b29939256f2f3e7b2
cutajarj2 <james.cutajar@blip.pt>
2019-07-11 14:11:03 +0100

    Adding maxsubarray array brute and topdown

---
+++ src/main/java/com/cutajarjames/dynamicprogramming/maxsubarry/MaxSubArrayRec.java
@@ -0,0 +1,35 @@
+package com.cutajarjames.dynamicprogramming.maxsubarry;
+
+public class MaxSubArrayRec {
+    private int[] prices;
+
+    public MaxSubArrayRec(int[] prices) {
+        this.prices = prices;
+    }
+
+    public int maxSubArray() {
+        int max = 0;
+        for (int j = 0; j < prices.length; j++) {
+            max = Math.max(max, this.maxSubArrayEndingAt(j));
+        }
+        return max;
+    }
+
+    public int maxSubArrayEndingAt(int i) {
+        if (i == 0) return prices[0];
+        return Math.max(prices[i], maxSubArrayEndingAt(i - 1) + prices[i]);
+    }
+
+    public static void main(String[] args) {
+        MaxSubArrayRec rec = new MaxSubArrayRec(new int[]{5, -4, 8, -10, -2, 4, -3, 2, 7, -8, 3, -5, 3});
+        System.out.println(rec.maxSubArray());
+        /*
+        int[] input = new int[30000];
+        Arrays.fill(input, 1);
+        for (int i = 0; i < 10; i ++) {
+            MaxSubArrayRec msa = new MaxSubArrayRec(input);
+            System.out.println(msa.maxSubArray());
+        }
+        */
+    }
+}
---
+++ src/main/java/com/cutajarjames/dynamicprogramming/maxsubarry/MaxSubArrayTopDown.java
@@ -0,0 +1,43 @@
+package com.cutajarjames.dynamicprogramming.maxsubarry;
+
+import java.util.Arrays;
+
+public class MaxSubArrayTopDown {
+    private int[] prices;
+    private int[] subSolutions;
+
+    public MaxSubArrayTopDown(int[] prices) {
+        this.prices = prices;
+        subSolutions = new int[prices.length];
+        Arrays.fill(subSolutions, -1);
+    }
+
+    public int maxSubArray() {
+        int max = 0;
+        for (int j = 0; j < prices.length; j++) {
+            max = Math.max(max, this.maxSubArrayEndingAt(j));
+        }
+        return max;
+    }
+
+    public int maxSubArrayEndingAt(int i) {
+        if (subSolutions[i] != -1) return subSolutions[i];
+        if (i == 0) return prices[0];
+        int max = Math.max(prices[i], maxSubArrayEndingAt(i - 1) + prices[i]);
+        subSolutions[i] = max;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        MaxSubArrayTopDown msa = new MaxSubArrayTopDown(new int[]{5, -4, 8, -10, -2, 4, -3, 2, 7, -8, 3, -5, 3});
+        System.out.println(msa.maxSubArray());
+        /*
+        int[] input = new int[30000];
+        Arrays.fill(input, 1);
+        for (int i = 0; i < 10; i ++) {
+            MaxSubArrayTopDown msa = new MaxSubArrayTopDown(input);
+            System.out.println(msa.maxSubArray());
+        }
+         */
+    }
+}
